4525c0f3c1770f8aeee7ed025e2b81b9a6ecd4d1
wzp <1123715395@qq.com>
2020-08-05 20:57:46 +0800

    init

---
+++ src/main/java/com/ouke/ouke/OukeApplication.java
@@ -0,0 +1,13 @@
+package com.ouke.ouke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OukeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ouke/ouke/OukeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouke.ouke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
